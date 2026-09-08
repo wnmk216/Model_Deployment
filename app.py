54f23777c7e61965d461cbdb9dd5584a2fb2d651
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-09-08 10:09:55 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+
+# Title
+st.title("Web Application for Model Deployment)
+st.title("Data Science Group:blue[Applied Machine Learning Class 1/2569] :sunglasses:")
+st.title(":red[Created by ] :orange[Wanthanee] :green[Prachuabsupakij] :blue[..]:blossom:")
