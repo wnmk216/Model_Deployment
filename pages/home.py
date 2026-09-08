6dfc05975702d296a286707100a49a544d7dd545
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-09-08 15:57:47 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -6,5 +6,6 @@ import streamlit as st
 
 # Title
 st.title("Web Application for Model Deployment")
-st.header("Data Science Group:blue[Applied Machine Learning Class 1/2569] :sunglasses:")
+st.header("Data Science Group")
+st.subheader(":blue[Applied Machine Learning Class 1/2569] :sunglasses:")
 st.subheader(":red[Created by ] :orange[Wanthanee] :green[Prachuabsupakij] :blue[..]:blossom:")
