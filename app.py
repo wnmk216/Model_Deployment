34fa9ba26647ed2f7ed9a83546f29e1cbb75b8be
wnmk216 <wanthanee.p@itm.kmutnb.ac.th>
2026-09-13 14:04:39 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import plotly.express as px
 main_page = st.Page("pages/home.py", title="Main Page", icon="🎈")
 sale_predict = st.Page("pages/sale_predict.py", title="Regression", icon="❄️")
 cat_dog_classify = st.Page("pages/cat_dog_classification_app.py", title="Classification", icon="🎉")
-
+breeds_object_detection = st.Page("pages/object_detection_app.py", title="Detection", icon="🐶")
 # Set up navigation
-pg = st.navigation([main_page, sale_predict, cat_dog_classify])
+pg = st.navigation([main_page, sale_predict, cat_dog_classify,breeds_object_detection])
 
 # Run the selected page
 pg.run()
